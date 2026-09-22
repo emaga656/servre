c7b8b94cd50b66194a04e76f49ef3fab57306aaf
Евлоев Магомед АА <student175@ithub.local>
2026-09-22 11:03:07 +0300

    server

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+
+const PORT = 3000;
+
+app.get('/' , (req, res)=>{
+    res.send("Главная страница")
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Сервер успешно запушен по адресу: 127.0.0.1:${PORT}`);
+    
+})
